09e4033f487bc8733b46e8f0332ca7abd358d2e6
Clark Sell <clark.sell@csell.net>
2020-03-21 12:46:26 -0500

    migrates to express and pure node

--- perf_testing/test_run_autocannon.sh
+++ perf_testing/test_run_autocannon.sh
@@ -1,1 +1,1 @@
-autocannon -c 10 -m 'POST' -i test_event_query.json -H 'Content-Type: application/json' localhost:8000
+autocannon -c 10 -m 'POST' -i test_event_query.json -H 'Content-Type: application/json' http://localhost:8000/graphql
--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
-import apiHandler from './graphql';
+import 'dotenv/config';
+import express from 'express';
+import debug from 'debug';
+import { middleware } from '@thatconference/api';
 
-export const handler = apiHandler;
+import responseTime from 'response-time';
+import * as Sentry from '@sentry/node';
+import uuid from 'uuid/v4';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
+
+import graphServer from './server';
+import config from './envConfig';
+
+const dlog = debug('that:api:gateway:index');
+dlog('graph api started');
+const { requestLogger } = middleware;
+
+const api = express();
+
+Sentry.init({
+  dsn: config.sentry.dsn,
+  environment: config.sentry.environment,
+  release: config.sentry.release,
+  debug: config.sentry.debug,
+});
+
+Sentry.configureScope(scope => {
+  scope.setTag('thatApp', 'that-api-gateway');
+});
+
+function createUserContext(req, res, next) {
+  dlog('creating user context.');
+
+  const correlationId = req.headers['that-correlation-id']
+    ? req.headers['that-correlation-id']
+    : uuid();
+
+  Sentry.configureScope(scope => {
+    scope.setTag('correlationId', correlationId);
+  });
+
+  req.userContext = {
+    authToken: req.headers.authorization,
+    correlationId,
+    enableMocks: req.headers['that-enable-mocks']
+      ? req.headers['that-enable-mocks']
+      : false,
+  };
+
+  next();
+}
+
+async function schemaRefresh(req, res) {
+  dlog('Refreshing Gateway Schemas');
+  await graphServer.config.gateway.load();
+  res.json({ status: 'reloaded' });
+}
+
+function failure(err, req, res, next) {
+  dlog('middleware error %O', err);
+  Sentry.captureException(err);
+
+  res
+    .set('Content-Type', 'application/json')
+    .status(500)
+    .json(err);
+}
+
+api
+  .set('etag', false)
+  .use(responseTime())
+  .use(requestLogger('that:api:gateway').handler)
+  .use(createUserContext)
+  .use('/.internal/apollo/schema-refresh', schemaRefresh)
+  .use('/view', voyagerMiddleware({ endpointUrl: '/graphql' }))
+  .use(failure);
+
+graphServer.applyMiddleware({ app: api, path: '/' });
+api.listen({ port: 8000 });
