e43bddb2da7308b4b3d832b497ab05f6ddb2f3a3
Brett Slaski <brett@thatconference.com>
2020-03-27 16:51:45 -0500

    set for merge to master

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { middleware } from '@thatconference/api';
 
 import responseTime from 'response-time';
 import * as Sentry from '@sentry/node';
-import uuid from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
 import graphServer from './server';
@@ -33,7 +33,7 @@ function createUserContext(req, res, next) {
 
   const correlationId = req.headers['that-correlation-id']
     ? req.headers['that-correlation-id']
-    : uuid();
+    : uuidv4();
 
   Sentry.configureScope(scope => {
     scope.setTag('correlationId', correlationId);
