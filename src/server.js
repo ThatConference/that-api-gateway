ddbcfc5a3da5197ea23b97654951bdd6c5d1542c
Brett Slaski <brett@thatconference.com>
2023-02-24 16:16:48 -0600

    Updated to latest server version. This gateway will support v3 children refactor apollo server setup for apollo v4 add node 18 support Dockerfile runtime to 18

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import { isNil } from 'lodash';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground';
 import {
   ApolloGateway,
   IntrospectAndCompose,
   RemoteGraphQLDataSource,
 } from '@apollo/gateway';
-import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import debug from 'debug';
 
 import config from './envConfig';
@@ -14,7 +15,7 @@ const dlog = debug('that:api:gateway:graphql');
 
 class AuthenticatedDataSource extends RemoteGraphQLDataSource {
   constructor(url) {
-    dlog('AuthenticatedDataSource constructor');
+    dlog('AuthenticatedDataSource constructor on %s', url);
     super({ url });
   }
 
@@ -38,73 +39,78 @@ class AuthenticatedDataSource extends RemoteGraphQLDataSource {
         request.http.headers.set('that-forwarded-for', context.referer);
       }
       if (context.site) request.http.headers.set('that-site', context.site);
+
+      dlog('setting headers done: %o', request.http.headers);
     }
   }
 }
 
-const createGateway = new ApolloGateway({
-  supergraphSdl: new IntrospectAndCompose({
-    subgraphs: [
-      {
-        name: 'Events',
-        url: config.servicesList.events,
-      }, // port: 8001
-      {
-        name: 'Partners',
-        url: config.servicesList.partners,
-      }, // port: 8002
-      {
-        name: 'Sessions',
-        url: config.servicesList.sessions,
-      }, // port: 8003
-      {
-        name: 'Members',
-        url: config.servicesList.members,
-      }, // port: 8004
-      {
-        name: 'Garage',
-        url: config.servicesList.garage,
-      }, // port: 8005
-      {
-        name: 'Communications',
-        url: config.servicesList.communications,
-      }, // port: 8006
-    ],
-  }),
-
-  // for every child service we want to add information to the request header.
-  buildService({ name, url }) {
-    dlog(`composing federation: building schema for ${name} : ${url}`);
-    return new AuthenticatedDataSource(url);
-  },
-  debug: config.apollo.debug,
-  serviceHealthCheck: true,
-});
-
-const createServer = new ApolloServer({
-  gateway: createGateway,
-  cache: 'bounded',
-  csrfPrevention: true,
-  subscriptions: false,
-  engine: false,
-  introspection: config.apollo.introspection,
-  context: ({ req: { userContext }, res }) => ({ ...userContext, res }),
-  formatResponse: (response, { context: { res } }) => {
-    // dlog('response:: %O', response);
-    res.header({
-      'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-    });
-  },
-  plugins: [
-    ApolloServerPluginLandingPageGraphQLPlayground({
-      settings: {
-        'schema.polling.enable': false,
-        'schema.disableComments': false,
-      },
+function createGraphQLServer(httpServer) {
+  const createdGateway = new ApolloGateway({
+    supergraphSdl: new IntrospectAndCompose({
+      subgraphs: [
+        {
+          name: 'Events',
+          url: config.servicesList.events,
+        }, // port: 8001
+        {
+          name: 'Partners',
+          url: config.servicesList.partners,
+        }, // port: 8002
+        {
+          name: 'Sessions',
+          url: config.servicesList.sessions,
+        }, // port: 8003
+        {
+          name: 'Members',
+          url: config.servicesList.members,
+        }, // port: 8004
+        {
+          name: 'Garage',
+          url: config.servicesList.garage,
+        }, // port: 8005
+        {
+          name: 'Communications',
+          url: config.servicesList.communications,
+        }, // port: 8006
+      ],
     }),
-  ],
-});
 
-export default createServer;
+    // for every child service we want to add information to the request header.
+    buildService({ name, url }) {
+      dlog(`composing federation: building schema for ${name} : ${url}`);
+      return new AuthenticatedDataSource(url);
+    },
+    debug: config.apollo.debug,
+    serviceHealthCheck: true,
+  });
+
+  const createdServer = new ApolloServer({
+    gateway: createdGateway,
+    cache: 'bounded',
+    csrfPrevention: true,
+    subscriptions: false,
+    engine: false,
+    introspection: config.apollo.introspection,
+    formatResponse: (response, { context: { res } }) => {
+      res.header({
+        'Strict-Transport-Security': 'max-age=31536000; includeSubDomains',
+        'X-Frame-Options': 'DENY',
+        'X-Content-Type-Options': 'nosniff',
+      });
+    },
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageGraphQLPlayground({
+        settings: {
+          'schema.polling.enable': false,
+          'schema.disableComments': false,
+        },
+      }),
+    ],
+  });
+
+  return createdServer;
+}
+
+export default createGraphQLServer;
